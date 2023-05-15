98e7a2008c27a013c433c2b14f62920a34b6b958
farnazdelavari <48866546+farnazdelavari@users.noreply.github.com>
2023-05-15 11:30:56 +0200

    Update mCAP_w_find_activity.m

--- functions/mCAP_w_find_activity.m
+++ functions/mCAP_w_find_activity.m
@@ -121,3 +121,20 @@ function [Xonp,p,Indices,idx_sep_seeds,Xonp_scrub,S] = mCAP_w_find_activity(tcvo
     % Matrix containing all the interesting probabilities
     p = [p_scrubbed; p_scrubactive; p_active];
 end
+
+
+
+% Copyright 2023 Farnaz Delavari
+%
+% Licensed under the Apache License, Version 2.0 (the "License");
+%    you may not use this file except in compliance with the License.
+%    You may obtain a copy of the License at
+%
+%        http://www.apache.org/licenses/LICENSE-2.0
+%
+%    Unless required by applicable law or agreed to in writing, software
+%    distributed under the License is distributed on an "AS IS" BASIS,
+%   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%   See the License for the specific language governing permissions and
+%   limitations under the License.
+
