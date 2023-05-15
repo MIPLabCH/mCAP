23da64011968debbab9232f6db8f08993003dea4
farnazdelavari <48866546+farnazdelavari@users.noreply.github.com>
2023-05-15 11:27:22 +0200

    Update mCAP_main.m

--- mCAP_main.m
+++ mCAP_main.m
@@ -92,3 +92,21 @@ mkdir(save_dir);
 save(fullfile(save_dir,['mCAP_K',char(num2str(ik)),'.mat']),'mat_save','it','-v7.3'); clear mat_save it;
 fprintf(fid,['Done without errors...\n']);
 end
+
+
+
+
+% Copyright 2023 Farnaz Delavari
+%
+% Licensed under the Apache License, Version 2.0 (the "License");
+%    you may not use this file except in compliance with the License.
+%    You may obtain a copy of the License at
+%
+%        http://www.apache.org/licenses/LICENSE-2.0
+%
+%    Unless required by applicable law or agreed to in writing, software
+%    distributed under the License is distributed on an "AS IS" BASIS,
+%   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%   See the License for the specific language governing permissions and
+%   limitations under the License.
+
